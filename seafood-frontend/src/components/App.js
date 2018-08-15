df7292884ea98b2b009addc944f2e5eb02245035
Szymon <tosikszymon@gmail.com>
2018-08-15 15:32:21 +0200

    Added basic template for frontend

---
+++ seafood-frontend/src/components/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
+
+export class App extends Component {
+    render() {
+        return (
+            <div className="catch-of-the-day">'
+                <div className="menu">
+                    <Header tagline="Fresh Seafood Market"/>
+                </div>
+                <Order/>
+                <Inventory/>
+            </div>
+        )
+    }
+}
+
+export default App
