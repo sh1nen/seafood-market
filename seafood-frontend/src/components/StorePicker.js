42f56bb96168a1d94f961a43276ae1ee423dbc47
Szymon <tosikszymon@gmail.com>
2018-08-15 20:12:48 +0200

    Applied helper fun for generating random name

--- seafood-frontend/src/components/StorePicker.js
+++ seafood-frontend/src/components/StorePicker.js
@@ -1,15 +1,12 @@
-import React, {Component} from 'react'
+import React from 'react'
+import { getFunName } from './../helpers';
 
-export class StorePicker extends Component {
-  render() {
-    return (
-      <form className="store-selector">
-        <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name"/>
-        <button type="submit">Visit Store</button>
-      </form>
-    )
-  }
-}
+const StorePicker = () => (
+  <form className="store-selector">
+    <h2>Please Enter a Store</h2>
+    <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+    <button type="submit">Visit Store</button>
+  </form>
+);
 
 export default StorePicker;
