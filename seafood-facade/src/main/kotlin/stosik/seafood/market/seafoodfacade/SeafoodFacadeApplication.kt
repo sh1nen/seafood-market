b06625f939687ad6c88d5d0ce1936180fdfd7aac
Szymon <tosikszymon@gmail.com>
2018-08-15 15:57:04 +0200

    Added backend structure

---
+++ seafood-facade/src/main/kotlin/stosik/seafood/market/seafoodfacade/SeafoodFacadeApplication.kt
@@ -0,0 +1,11 @@
+package stosik.seafood.market.seafoodfacade
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SeafoodFacadeApplication
+
+fun main(args: Array<String>) {
+    runApplication<SeafoodFacadeApplication>(*args)
+}
