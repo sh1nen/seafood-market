b195d09548b95596eaac0475aaf94dc162d0f9d4
Szymon <tosikszymon@gmail.com>
2018-08-16 15:18:23 +0200

    Added routing to store

--- seafood-frontend/src/components/StorePicker.js
+++ seafood-frontend/src/components/StorePicker.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import { getFunName } from './../helpers';
+import React, { Component } from "react";
+import { getFunName } from "./../helpers";
 
-const StorePicker = () => (
-  <form className="store-selector">
-    <h2>Please Enter a Store</h2>
-    <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
-    <button type="submit">Visit Store</button>
-  </form>
-);
+export class StorePicker extends Component {
+  storeNameInput = React.createRef();
+
+  goToStore = event => {
+    event.preventDefault();
+    const storeName = this.storeNameInput.value.value;
+    this.props.history.push(`/store/${storeName}`);
+  };
+
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter a Store</h2>
+        <input
+          type="text"
+          required
+          placeholder="Store Name"
+          ref={this.storeNameInput}
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store</button>
+      </form>
+    );
+  }
+}
 
 export default StorePicker;
